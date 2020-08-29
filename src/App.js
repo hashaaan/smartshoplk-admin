433c7275052bd16e1bd63ea8c59e6681100238a0
Mohamed Minsaaf <minsafmohamed7@gmail.com>
2020-08-29 10:39:25 +0530

    updated dashboard

--- src/App.js
+++ src/App.js
@@ -15,34 +15,65 @@ import EditSmartphone from "./Components/EditSmartphone";
 
 function App() {
   return (<>
-    <nav className="navbar navbar-dark bg-dark">
-  
-   
-  <a className="navbar-brand" href="#">Navbar</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="Components/CreateSmartphone">Features</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Pricing</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
+   <Router>
+    <div className="App">
+      <header className="App-header">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+
+            <Navbar.Brand>
+              <Link to="/CreateSmartphone" className="nav-link">
+                SmartPhone.lk Dashboard
+              </Link>
+            </Navbar.Brand>
+
+            <Nav className="justify-content-end">
+              <Nav>
+                <Link to={"/CreateSmartphone"} className="nav-link">
+                  Create Smartphone
+                </Link>
+              </Nav>
+
+          
+
+              <Nav>
+                <Link to={"/EditSmartphone"} className="nav-link">
+                  Edit Smartphone
+                </Link>
+                
+              </Nav>
+              <Nav>
+                <Link to={"/student-list"} className="nav-link">
+                  View Smartphone
+                </Link>
+            </Nav>
+            </Nav>
+
+          </Container>
+        </Navbar>
+      </header>
+
+      <Container>
+        <Row>
+          <Col md={12}>
+            <div className="wrapper">
+              <Switch>
+                <Route exact path='/CreateSmartphone' component={CreateSmartphone} />
+                <Route path="/EditSmartphone" component={EditSmartphone} />
+                <Route path="/edit-student/:id" component={EditSmartphone} />
+                <Route path="/student-list" component={EditSmartphone} />
+              </Switch>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  </Router>
 
 
 
 
-</nav>
+
 </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import CreateSmartphone from "./Components/CreateSmartphone"
 ReactDOM.render(
   <BrowserRouter>
    <App />
-   <CreateSmartphone/>
+  
   </BrowserRouter>
    
   ,
