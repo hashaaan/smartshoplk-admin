ba9f372649fcc55f517c5cd9c6e6b7d3d910af3c
Mohamed Minsaaf <minsafmohamed7@gmail.com>
2020-08-29 10:28:17 +0530

    Added admin UIs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import React from "react";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
+
+import CreateSmartphone from "./Components/CreateSmartphone";
+import EditSmartphone from "./Components/EditSmartphone";
+
 
 function App() {
-  return (
-    <div className="container">
-      <h1 className="heading-1">Welcome to Smartshop.lk Admin Dashboard!</h1>
-    </div>
+  return (<>
+    <nav className="navbar navbar-dark bg-dark">
+  
+   
+  <a className="navbar-brand" href="#">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="Components/CreateSmartphone">Features</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Pricing</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+
+
+
+
+</nav>
+</>
   );
 }
 
---
+++ src/Components/EditSmartphone.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+ class EditSmartphone extends Component {
+  render() {
+    return (
+      <div>
+        <p>React Edit Student Component!</p>
+      </div>
+    );
+  }
+}
+
+
+export default EditSmartphone;
